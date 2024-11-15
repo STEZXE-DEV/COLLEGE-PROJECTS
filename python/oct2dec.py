00e65d4e7ca38a1fa402d953336a23fbb49b2f45
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-15 17:25:22 +0100

    Octadecimal to Decimal Converter 1.0

---
+++ python/oct2dec.py
@@ -0,0 +1,50 @@
+while True:
+    liczba = str(input("Podaj liczbe w postaci osemkowej: \n"))
+    postac_oct = list(liczba)
+    error=0
+    error_alert=[]
+    dlugosc=len(postac_oct)
+    wynik=[]
+    if dlugosc>32:
+        error+=1
+        error_alert.append("PRZEKROCZONO ZAKRES WYSWIETLANYCH ZNAKOW!!!")
+        for i in postac_oct:
+            if i.isdigit():
+                if int(i) == 8 or int(i) == 9:
+                    error+=1
+                    error_alert.append("TO NIE JEST LICZBA W POSTACI OSEMKOWEJ!!!")
+                    break
+            else:
+                error+=1
+                error_alert.append("TO NIE JEST LICZBA!!!")
+                break
+    elif dlugosc<=32:
+        for i in postac_oct:
+            if i.isdigit():
+                if int(i) == 8 or int(i) == 9:
+                    error+=1
+                    error_alert.append("TO NIE JEST LICZBA W POSTACI OSEMKOWEJ!!!")
+                else:
+                    liczba_dec=int(i)*8**(dlugosc-1)
+                    wynik.append(int(liczba_dec))
+                    dlugosc-=1
+                    if dlugosc==0:
+                        break
+            else:
+                error+=1
+                error_alert.append("TO NIE JEST LICZBA!!!")
+                break
+    if error>0:
+        print("\n======================ERROR====================")
+        print(f"Liczba napotkanych bledow: {error}")
+        print("==================ERROR_ALERT==================")
+        for k in error_alert:
+            print(k,sep='\n')
+        print("======================ERROR====================\n")
+    elif error==0:
+        suma=str(sum(wynik))
+        zakres=len(suma)
+        print("\n=================WYNIK_DEC===============")
+        print(sum(wynik))
+        print(f"Zakres wyswietlanych znakow: {zakres}")
+        print("=========================================")
