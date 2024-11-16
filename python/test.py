940d22ea128650a4909470239ff8b8ee073d0b2c
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-16 10:04:53 +0100

    testing...

--- python/test.py
+++ python/test.py
@@ -1,3 +1,33 @@
 
 print("error: integer out of context ")
-print(__name__)  
+print(__name__) 
+
+# zmienna
+# @brief:
+# @paraf:
+# @paraf:
+
+for cyfry in range(0,10):
+    print(cyfry, end='')
+
+a=5
+a=None
+print(f"\n{a}")
+
+# async - asynchroniczna procedra
+# wyjatki (try, except, raise, finally)
+# funkcje (def i return)
+# return
+# pass
+# lambda
+# class
+
+# symbol _ uzywa sie jako placeholder
+for _ in range(1,10):
+    print("TEST", end='')
+
+a=(1,2,3,4)
+
+c,_,_,_= a
+print("\n",c)
+
