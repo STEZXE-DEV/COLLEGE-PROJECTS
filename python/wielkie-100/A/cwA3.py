e987aeacd0042b90026454b5387282c044116efb
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-18 13:58:23 +0100

    Update cwA3.py
    Added comments for better understanding of the code.

--- python/wielkie-100/A/cwA3.py
+++ python/wielkie-100/A/cwA3.py
@@ -1,5 +1,5 @@
 liczby_pierwsze=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
-for i in liczby_pierwsze:
-    print("\t",i)
+for i in liczby_pierwsze: #i jest pojedynczym elementem listy,
+    print("\t",i)   #dlatego funkcja for wypisuje wraz z \t (tabulacja jest równa czterem spacjom) każdy element z listy 
 
 
