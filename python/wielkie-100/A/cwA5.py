29b161a599824b1f1d3dd47856b224d540ed57ad
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-18 14:58:44 +0100

    Update cwA5.py

--- python/wielkie-100/A/cwA5.py
+++ python/wielkie-100/A/cwA5.py
@@ -3,7 +3,7 @@ b=17.5
 c=-10**20
 #formatowanie str.format()
 print("{:5d}".format(a)) #d liczba calkowita (np. int)
-print("{:7f}".format(b)) #liczba zmiennoprzecinkowa (np. float)
+print("{:7d}".format(a)) #na siedmiu polach
 print("{:7.3f}".format(b)) #7- ilosc pol, .3 - ilosc pol po przecinku
 print("{:e}".format(c)) #formatowanie naukowe
 print("{:g}".format(c)) #formatowanie naturalne
