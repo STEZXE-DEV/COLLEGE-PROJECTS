400d8972cb1ad827eb0e11a0648bede6c90edfc7
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-18 13:47:12 +0100

    Update cwA4.py
    Added comments for better understanding of the code.

--- python/wielkie-100/A/cwA4.py
+++ python/wielkie-100/A/cwA4.py
@@ -1,7 +1,8 @@
 a=15
 b=17.5
-c=-10**20
-print("%5d" % a )
-print("%07d" % b)
-print("%e" % c)
-print("%g" % c)
+c=-10**20 #-10 do potegi 20
+print("%5d" % a ) #wypelnianie pustymi miejscami z lewej strony jesli zmienna nie przekroczy 5 znakow
+print("%07.1f" % b) #wypelnianie zerami z lewej strony jesli zmienna nie przekroczy 7 znakow
+print("%0.3f" % b)  #zmienna z 3-ma miejscami po przecinku
+print("%e" % c) #formatownie naukowe liczb
+print("%g" % c) #formatownie naturalne liczb
