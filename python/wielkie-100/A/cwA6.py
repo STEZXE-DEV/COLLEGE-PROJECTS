f44ce9f2fcd2b608eb49c6b81503cd712b5cb41b
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-18 15:01:57 +0100

    cwA6.py (with comments)

---
+++ python/wielkie-100/A/cwA6.py
@@ -0,0 +1,8 @@
+a=15
+b=17.5
+c=-10**20
+print(f"{a:5d}") #wypelnianie pustych miejsc spacjami
+print(f"{a:07d}") #wypelnianie pustych miejsc zerami
+print(f"{b:7.3f}") #7 miejsc i 3 po przecinku
+print(f"{a:e}") #naukowe
+print(f"{a:g}") #naturalne
