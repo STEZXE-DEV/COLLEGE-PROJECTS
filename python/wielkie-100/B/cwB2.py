5108a828ffb16cbf4f24b58cba736b13dfca5449
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-18 22:48:33 +0100

    cwB2.py Completed (with comments)

---
+++ python/wielkie-100/B/cwB2.py
@@ -0,0 +1,3 @@
+a = int(input("podaj pierwsza liczbe: \n"))  #wejscie jako int
+b = float(input("podaj druga liczbe: \n"))  #wejscie jako float
+print(f"{a+15}\n{b-7.2}")   #wypisanie 1 liczby powiekszonej o 15 i drugiej pomniejszonej o 7.2
