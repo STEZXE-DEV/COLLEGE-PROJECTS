da6c6ef43f3d235d5dadef386a0c26e702b58905
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-18 19:58:04 +0100

    cwB1.py Completed (with comments)

---
+++ python/wielkie-100/B/cwB1.py
@@ -0,0 +1,3 @@
+a = input("Podaj tekst: \n") #pobranie wejscia od uzytkownika
+for i in range(1,4): #zakres od jeden do trzy
+    print(f"Linijka tekstu {i}: {a}") #wypisanie z liczeniem linijek tekstu
