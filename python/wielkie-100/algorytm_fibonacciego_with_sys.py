cd0f658df3484209446103db8e6ff4335d5e1539
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-23 15:00:39 +0100

    Fibonacci algorithm

---
+++ python/wielkie-100/algorytm_fibonacciego_with_sys.py
@@ -0,0 +1,28 @@
+import sys
+sys.set_int_max_str_digits(100000000) #uzywac na wlasna odpowiedzialnosc - nie odpowiadam za eksplozje procesora
+
+
+def wylicz_element_ciagu(n):
+
+    #Funkcja wylicza n-ty wyraz ciagu Fibonacciego
+    #arg: n - indeks liczby Fibonacciego
+
+    F0=0
+    F1=1
+
+    for x in range(2,n+1):
+        F=F1+F0
+        F0=F1
+        F1=F
+    
+    return F
+while True:
+    #n=int(input("Podaj indeks liczby fibonacciego: \n"))
+    if len(sys.argv)>1:
+        n = int(sys.argv[1])
+    else:
+        print("Ten program wymaga argumentu N")
+        print("Usage "+sys.argv[0]+"N")
+        exit()
+    F = wylicz_element_ciagu(n)
+    print(F)
