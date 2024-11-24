cd79dcde6b31fdb2bc89f82341a80e179a07a73c
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-24 18:42:05 +0100

    work in progress...

---
+++ python/wielkie-100/B/cwB6.py
@@ -0,0 +1,5 @@
+import pyinputplus as pip
+
+wejscie = pip.inputStr(prompt="Podaj dane w formacie: <zp>, ˽<sp>: <sp1> ˽ <sp2> ˽ <sp1> ˽ <sp2>,<s>\n")
+
+#no idea how to do that, surely will return to this later :)
