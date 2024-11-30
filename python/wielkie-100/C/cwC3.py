39e21fd1b6691d0163d64becb767c31b1fa77013
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-11-30 10:44:59 +0100

    cwC3.py Completed (with commentary) ver. 1.0

---
+++ python/wielkie-100/C/cwC3.py
@@ -0,0 +1,32 @@
+#import potrzebnych bibliotek i utworzenie aliasow <as>
+import numpy as np
+import pyinputplus as pyi
+import math as m
+
+#zmiennoprzecinkowa pojedynczej precyzji <numpy.float32>
+pi=np.float32(m.pi) #deklaracja liczby pi z modulu math
+
+#pobranie wejsc i konwersja na float32
+a=pyi.inputFloat(prompt="Podaj wartosc a: \n")
+a = np.float32(a)
+b=pyi.inputFloat(prompt="Podaj wartosc b: \n")
+b = np.float32(b)
+
+#wykonanie obliczen podanych w zadaniu
+obl1=m.sin(a)
+obl2=m.cos(b)
+obl3=m.cos(a)+m.cos(b)
+obl4=m.radians(a/pi)
+obl5=m.degrees(b*pi)
+wynik = [obl1,obl2,obl3,obl4,obl5] #lista z wynikami obliczen
+dl=len(wynik) #dlugosc listy z wynikami
+
+#wypisanie wynikow w petli for,
+#ograniczone wyswietlanie liczby do czterech miejsc po przecinku <zmienna:.4f> f - liczba typu float
+for i in range(0, dl):
+    if i==3: #obliczenie IV
+        print(f"Wynik {i+1}: {wynik[i]:.4f} radian/y/ow.")
+    elif i==4: #obliczenie V
+        print(f"Wynik {i+1}: {wynik[i]:.4f} stopni.")
+    else: #reszta wynikow
+        print(f"Wynik {i+1}: {wynik[i]:.4f}.")
