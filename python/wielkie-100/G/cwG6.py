70182c0b6b5b676d1ba7822b802b5706d2838bfa
ST9-DEV <stanislawjurek.ina@gmail.com>
2024-12-21 13:49:14 +0100

    "cwG6.py" (Completed - with documentation) <ver. 1.1>
    Otrzymywanie liczby lustrzanej w różnych systemach liczbowych.

--- python/wielkie-100/G/cwG6.py
+++ python/wielkie-100/G/cwG6.py
@@ -8,50 +8,19 @@ def pobierz_dane():
     return inp
 
 
-#funkcja, która dokonuje odbicia lustrzanego liczby w sys. dz.,
+#funkcja, która dokonuje odbicia lustrzanego liczby w każdym systemie,
 #a następnie weryfikuje czy liczby (stringi) są sobie równe,
 #zwraca uzyskaną liczbę lustrzaną lub <"Nie istnieje!">
 
-def dziesietna_liczba_lustrzana(a):
+def liczba_lustrzana(a):
     x=[]
-    for i in str(a):
+    a=str(a)
+    for i in a:
         x.append(i)
     x.reverse()
-    b=int(''.join(x))
+    b=''.join(x)
     if a==b:
         return b
-    else:
-        return "Nie istnieje!"
-
-
-#funkcja, która dokonuje odbicia lustrzanego liczby w sys. bin.,
-#a następnie weryfikuje czy liczby (stringi) są sobie równe,
-#zwraca uzyskaną liczbę lustrzaną lub <"Nie istnieje!">
-
-def binarna_liczba_lustrzana(a):
-    x=[]
-    for i in a:
-        x.append(i)
-    x.reverse()
-    y=''.join(x)
-    if y==a:
-        return y
-    else:
-        return "Nie istnieje!"
-
-
-#funkcja, która dokonuje odbicia lustrzanego liczby w sys. hex.,
-#a następnie weryfikuje czy liczby (stringi) są sobie równe,
-#zwraca uzyskaną liczbę lustrzaną lub <"Nie istnieje!">
-
-def heksadecymalna_liczba_lustrzana(a):
-    x=[]
-    for i in a:
-        x.append(i)
-    x.reverse()
-    y=''.join(x)
-    if y==a:
-        return y
     else:
         return "Nie istnieje!"
 
@@ -61,17 +30,17 @@ def heksadecymalna_liczba_lustrzana(a):
 def wyswietl():
 
     liczba_dz=pobierz_dane()
-    mirror_dz=dziesietna_liczba_lustrzana(liczba_dz)
+    mirror_dz=liczba_lustrzana(liczba_dz)
 
     liczba_bin=bin(liczba_dz)
-    mirror_bin=binarna_liczba_lustrzana(liczba_bin[2:])
+    mirror_bin=liczba_lustrzana(liczba_bin[2:])
 
     liczba_hex=hex(liczba_dz)
-    mirror_hex=heksadecymalna_liczba_lustrzana(liczba_hex[2:])
+    mirror_hex=liczba_lustrzana(liczba_hex[2:])
 
     print(f"Podana liczba: {liczba_dz} | Liczba lustrzana: {mirror_dz}")
     print(f"Podana liczba w sys. bin.: {liczba_bin[2:]} | Liczba do niej lustrzana: {mirror_bin}")
-    print(f"Podana liczba w sys. hex.: {liczba_hex[2:]} | Liczba do niej lustrzana: {mirror_hex}")
+    print(f"Podana liczba w sys. hex.: {liczba_hex[2:].upper()} | Liczba do niej lustrzana: {mirror_hex.upper()}")
     
 if __name__=="__main__":
     wyswietl()
